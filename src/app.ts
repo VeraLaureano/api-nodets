fb17945d6bf9f7094b601793f32b44b43303f562
VeraLaureano <laureanoivangerardo@gmail.com>
2023-07-05 11:09:24 -0300

    ep 3

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { NODE_ENV, PORT } from "./config/config";
+import { API_VERSION, NODE_ENV, PORT } from "./config/config";
 import { Routes } from "./interfaces/route.interface";
 import { logger } from './utils/loggers';
+import displayRoutes from 'express-routemap';
 
 class App {
     public app: express.Application;
@@ -12,14 +13,23 @@ class App {
         this.app = express();
         this.env = NODE_ENV || "development";
         this.port = Number(PORT) || 5000;
+
+        this.initializeRoutes(routes);
+    }
+
+    public initializeRoutes(routes: Array<Routes>) {
+        routes.forEach((route) => {
+            this.app.use(`/api/${API_VERSION}`, route.router);
+        })
     }
 
     public listen() {
         this.app.listen(this.port, () => {
-            logger.info('=============================');
-            logger.info('====== ENV: ${this.env} =====');
-            logger.info('Server listening on port ${this.port}');
-            logger.info('=============================');
+            displayRoutes(this.app);
+            logger.info(`=============================`);
+            logger.info(`====== ENV: ${this.env} =====`);
+            logger.info(`Server listening on port ${this.port}`);
+            logger.info(`=============================`);
         })
     }
     
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import  App  from './app';
-import { NODE_ENV, PORT } from './config/config';
 import BaseRoute from './routes/base.route';
 
 const app = new App([new BaseRoute()])
